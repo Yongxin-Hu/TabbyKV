c89602bff73698f864b122e81da7f1363c08e343
Yongxin <849229520@qq.com>
2024-03-01 00:03:19 +0800

    update concat_iterator

--- src/engines/lsm/iterators/concat_iterator.rs
+++ src/engines/lsm/iterators/concat_iterator.rs
@@ -3,8 +3,9 @@ use crate::engines::lsm::iterators::StorageIterator;
 use crate::engines::lsm::table::iterator::SsTableIterator;
 use crate::engines::lsm::table::SsTable;
 use anyhow::Result;
+use bytes::Bytes;
 
-
+// 合并 key 没有重叠的 sstable_iterator, 减少不必要的 IO
 pub struct SstConcatIterator {
     current: Option<SsTableIterator>,
     next_sst_idx: usize,
@@ -16,7 +17,7 @@ impl SstConcatIterator {
         unimplemented!()
     }
 
-    pub fn create_and_seek_to_key(sstables: Vec<Arc<SsTable>>, key: KeySlice) -> Result<Self> {
+    pub fn create_and_seek_to_key(sstables: Vec<Arc<SsTable>>, key: Bytes) -> Result<Self> {
         unimplemented!()
     }
 }
